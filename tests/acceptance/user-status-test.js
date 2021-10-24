21599c0df8d5228ee369184eb10f5dbcbb9b7eb3
Nikhil Bhandarkar <nikhilbhandarkar1998@gmail.com>
2021-10-24 18:45:59 +0530

    Add tests for OOO button | import click in button test

--- tests/acceptance/user-status-test.js
+++ tests/acceptance/user-status-test.js
@@ -1,38 +1,57 @@
 import { module, test } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
+import { click, visit, currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
-import { click } from '@ember/test-helpers';
 
-module('Acceptance | user status', function(hooks) {
+module('Acceptance | user status', function (hooks) {
   setupApplicationTest(hooks);
 
-  test('visiting /user-status', async function(assert) {
+  test('Should update the status on button clicks', async function (assert) {
     await visit('/user-status');
+    assert.equal(currentURL(), '/user-status');
 
     assert
-      .dom('[data-test-status]')
+      .dom('[data-test-text="status"]')
       .hasText('I am idle', 'The user sees the correct status.');
 
     assert
-      .dom('[data-test-button-text]')
+      .dom('[data-test-button="update-status"]')
       .hasText(
         'Change status to ‘Doing a task’',
         'The user sees the correct button text.'
       );
 
-    await click('[data-test-button]');
+    await click('[data-test-button="update-status"]');
 
     assert
-      .dom('[data-test-status]')
+      .dom('[data-test-text="status"]')
       .hasText('I am doing a task', 'The user sees the correct status.');
 
     assert
-      .dom('[data-test-button-text]')
+      .dom('[data-test-button="update-status"]')
       .hasText(
         'Change status to ‘Idle’',
         'The user sees the correct button text.'
       );
 
-    assert.equal(currentURL(), '/user-status');
+    // OOO button
+    assert
+      .dom('[data-test-button="ooo"]')
+      .hasText(
+        'Mark yourself as OOO',
+        'The user sees the correct text for OOO button.'
+      );
+
+    await click('[data-test-button="ooo"]');
+
+    assert
+      .dom('[data-test-text="status"]')
+      .hasText('I am OOO', 'The user sees the correct status.');
+
+    assert
+      .dom('[data-test-button="ooo"]')
+      .hasText(
+        'Mark yourself as Active again',
+        'The user sees the correct button text.'
+      );
   });
 });
--- tests/integration/components/button-test.js
+++ tests/integration/components/button-test.js
@@ -1,12 +1,12 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { click, render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | button', function(hooks) {
+module('Integration | Component | button', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('It renders with arguments', async function(assert) {
+  test('Should render with arguments', async function (assert) {
     assert.expect(2);
 
     this.set('buttonText', 'Click me');
@@ -15,9 +15,15 @@ module('Integration | Component | button', function(hooks) {
       assert.ok(true, 'Closure action after click on item was executed.');
     });
 
-    await render(hbs`<Button @text={{buttonText}} @onClick={{onClick}} />`);
+    await render(hbs`
+      <Button @text={{this.buttonText}} @onClick={{this.onClick}} />
+    `);
 
-    assert.equal('[data-test-button]', 'Click me', 'Button has correct text.');
+    assert.equal(
+      this.element.textContent.trim(),
+      'Click me',
+      'Button has correct text.'
+    );
 
     await click('[data-test-button]');
   });
